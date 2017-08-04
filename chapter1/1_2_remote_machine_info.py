a794d1a82980b6124bc8ff350398fb110f43b2fc
tianjigezhu <tianjigezhuhn@aliyun.com>
2017-08-05 03:00:15 +0800

    新增第二个例子

--- chapter1/1_2_remote_machine_info.py
+++ chapter1/1_2_remote_machine_info.py
@@ -5,12 +5,11 @@
 
 import socket
 
-def get_remote_machine_info():
-	remote_host = 'www.python.org'
-	try:
-		print "IP address: %s" % socket.gethostbyname(remote_host)
-	except socket.error, err_msg:
-		print "%s: %s": % (remote_host, err_msg)
+def print_machine_info():
+	host_name = socket.gethostname()
+	ip_address = socket.gethostbyname(host_name)
+	print "Host name: %s" % host_name
+	print "IP address: %s" % ip_address
 
 if __name__ == '__main__':
-	get_remote_machine_info()
+	print_machine_info()

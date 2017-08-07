427c2a43305362e0398939f6f40827e359e4851b
tianjigezhu <tianjigezhuhn@aliyun.com>
2017-08-08 07:54:03 +0800

    开始新的章节的学习

---
+++ chapter4/untitled.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# Python Network Programming Cookbook -- Chapter - 4
+# This program is optimized for Python 2.7.
+# It may run on any other version with/without modifications.
